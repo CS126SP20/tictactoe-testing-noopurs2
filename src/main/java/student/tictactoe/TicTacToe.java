64e80f2d39b490ef992fe1c676e34d30470d62cc
noopurs2 <noopurs2@illinois.edu>
2020-02-04 15:48:02 -0600

    empty board and a board without enough characters will pass all  simpleNoWinnerBoard test cases.

--- src/main/java/student/tictactoe/TicTacToe.java
+++ src/main/java/student/tictactoe/TicTacToe.java
@@ -3,7 +3,15 @@ package student.tictactoe;
 import student.tictactoe.Evaluation;
 
 public class TicTacToe {
+
+    private int[][] board = new int[3][3];
+
     public static Evaluation evaluateBoard(String boardState) {
+// given an empty board or baord that has less than 9 characters is an invalid input.
+        if (boardState.length() == 0 || boardState.length() != 9) {
+            return Evaluation.NoWinner;
+        }
+
         return Evaluation.NoWinner;
     }
 }
